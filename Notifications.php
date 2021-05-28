d8c87698ff7cc8989276e1f8487b36f8b10d7312
kassim0 <kassim_haddad@yahoo.fr>
2021-05-29 00:25:13 +0200

    Update Notifications.php

--- Notifications.php
+++ Notifications.php
@@ -21,7 +21,7 @@
  			});
  			
 	</script> 
-	<style type="text/css">
+	<!-- <style type="text/css">
 		h1 {
 			text-align: center;
 			color: white;
@@ -36,7 +36,7 @@
 			margin: auto;
 		
 		}
-	</style>
+	</style> -->
 	
 </head>
 
@@ -62,6 +62,7 @@
 		 </div>
  	</nav>
  	<h1>Vos critères</h1>
+ 	<form method="post" action="Notif.php">
  	<table border="1">
  	<tr>
 		<td>Que recherchez-vous ?:</td>
@@ -81,81 +82,7 @@
 		</td>
 	</tr>
 	</table>
-
-<?php
-
-$database = "eceshop";
-$db_handle = mysqli_connect('localhost', 'root', '');
-$db_found = mysqli_select_db($db_handle, $database);
-
-$choice = isset($_POST["choix"])? $_POST["choix"] : "";
-if (empty($choice)) {
-	$choice = 0;
-	}
-$choice = (int)$choice;
-$sql = "";
-
-if ($db_found) {
-	
-	switch ($choice) {
-		case 1:
-		$sql = "SELECT * FROM articles WHERE nom LIKE '%MEUBLE%'";
-		break;
-		case 2:
-		$sql = "SELECT * FROM articles WHERE nom LIKE '%ORDINATEUR%'";
-		break;
-		case 3:
-		$sql = "SELECT * FROM articles WHERE nom LIKE '%COLLIER%'";
-		break;
-		case 4:
-		$sql = "SELECT * FROM articles WHERE nom LIKE '%VOITURE%'";
-		break;
-		case 5:
-		$sql = "SELECT * FROM articles WHERE nom LIKE 'T%'";
-		break;
-		case 6:
-		$sql = "SELECT * FROM articles WHERE nom LIKE '%CAHIER%'";
-		break;
-		case 7:
-		$sql = "SELECT * FROM articles WHERE nom LIKE '%MONTRE%'";
-		break;
-		default:
-		$sql = "SELECT * FROM articles WHERE nom LIKE 'T%'";
-		break;
-}
-echo "<h2>Voici votre sélection</h2>";
-
-echo "<p> Résultat:</p>";
-$result = mysqli_query($db_handle, $sql);
-
-echo '<div class="container">';
-echo '<div class="row">';
-while ($data = mysqli_fetch_assoc($result)) {
-echo	'<div class="col-lg-4">';	
-echo		'<div class="content">';
-echo			'<a class="article" href="pageArticle.php">';
-echo		 	'<img src = "data:image/jpd;base64,' . base64_encode($data['image']) . '" width = "75%"/>';
-echo           '</a>';
-echo		 '<h5>'.$data['nom'].'</h5>';
-echo		$data['prix'].'€';
-echo 		'  <button type="submit" name="add" class="btn btn-danger my-3">Ajouter au panier <i class="fas fa-shopping-cart"></i></button>';
-echo         '<input type="hidden" name="idarticle" >';
-		
-echo		'</div>';
-echo	'</div>';
-
-
-}
-echo 	'</div>';
-echo'</div>';
-
-} else {
-echo "<br>Votre produits n'est pas disponible, vous pouvez activer la cloche de notification qui vous informera s'il est à nouveau disponible";
-}
-
-
-mysqli_close($db_handle);
-?>
+	</form>
 
 
 	<footer class="page-footer">
