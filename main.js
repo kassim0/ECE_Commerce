92ef40f4f4f42ad3d36ffcc6d9dc8ed31932e68f
kassim0 <kassim_haddad@yahoo.fr>
2021-05-28 17:50:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         var signButton = $(this).html();
         console.log(signButton);
         debugger;
+        if(signButton == 'se Connecter'){
             $('.sign_up').html("se connecter"); // sign up button text change
             $('.sign_in').html("S'inscrire"); // sign in button text change
             $('.swift_right').show(); // second logo show
@@ -16,7 +17,21 @@ $(document).ready(function(){
             $('.username').hide(); // form user field hide
             $('.b-wrapper').addClass('swift_element'); // add reverse
             $('.adress').hide();
-        
+        }else{
+            $('.sign_up').html("s'inscrire"); // sign up button text change
+            $('.sign_in').html('se Connecter'); // sign in button text change
+            $('.swift_right').hide(); // second logo hide
+            $('.b-forgot').hide(); // forgot option hide
+            $('.form_title').html('Crée un compte');
+            $('.user_title').html('Connectez-vous ici');
+            //$('.user_subTitle').html('Ou connectez vous');
+            $('.b-title').css('margin-top','0px');
+            $('.swift_left').show();
+            $('.username').show();
+            $('.b-wrapper').removeClass('swift_element');
+            $('.adress').show();
+            $('.phone').show();
+        }
 
     })
 })
