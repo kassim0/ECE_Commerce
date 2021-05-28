eed1e839d88211cc9f07da445194fcd69ce4f677
kassim0 <kassim_haddad@yahoo.fr>
2021-05-28 18:22:56 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -23,46 +23,46 @@
 	$db_handle=mysqli_connect('localhost',"root",'');
 	$db_found= mysqli_select_db($db_handle, $database);
 
-	// 	if(isset($_POST["signup"])){
+		if(isset($_POST["signup"])){
 	
 		
-	// 	//echo $nb."<br>";
-	// 	if($db_found){
-	// 		//echo "fichier trouvé<br>";
-	// 		$sql="SELECT email,mdp FROM acheteur";
-	// 		$result = mysqli_query($db_handle, $sql);
-	// 		while($data = mysqli_fetch_assoc($result)){
+		//echo $nb."<br>";
+		if($db_found){
+			//echo "fichier trouvé<br>";
+			$sql="SELECT email,mdp FROM acheteur";
+			$result = mysqli_query($db_handle, $sql);
+			while($data = mysqli_fetch_assoc($result)){
 
-	// 			if($data['email']==$email){
-	// 				//echo "Email valide<br>";
+				if($data['email']==$email){
+					//echo "Email valide<br>";
 
-	// 				if($data['mdp']==$mdp){
-	// 					//echo "mot de passe valide<br>";
-	// 					$nb=1;
-
-
-	// 				}//else{echo "mot de passe invalide<br>";}
-
-	// 			}//else{echo "Email invalide<br>";}
-
-	// 		}
+					if($data['mdp']==$mdp){
+						//echo "mot de passe valide<br>";
+						$nb=1;
 
 
+					}//else{echo "mot de passe invalide<br>";}
 
-	// 	}//else{echo "fichier introuvable<br>";}
+				}//else{echo "Email invalide<br>";}
+
+			}
 
 
 
-	// }//else{echo "bouton sign IN non appuyé<br>";}
-	// if($nb==0){
-	// 	echo "Email ou mot de passe invalide<br>";
-	// 	 echo "<a href='connexion.html'>"."retourner à la page de connexion"."</a>";
-	// 	//echo "retourner à la page de connexion";
-	// }
-	// else{
-	// 	echo "connexion réussie<br>";
-	// 	 echo "Cliquez "."<a href='compte.html'>"."ici"."</a>"." pour accéder à votre compte";
-	// }
+		}//else{echo "fichier introuvable<br>";}
+
+
+
+	}//else{echo "bouton sign IN non appuyé<br>";}
+	if($nb==0){
+		echo "Email ou mot de passe invalide<br>";
+		 echo "<a href='connexion.html'>"."retourner à la page de connexion"."</a>";
+		//echo "retourner à la page de connexion";
+	}
+	else{
+		echo "connexion réussie<br>";
+		 echo "Cliquez "."<a href='compte.html'>"."ici"."</a>"." pour accéder à votre compte";
+	}
 
 
 
