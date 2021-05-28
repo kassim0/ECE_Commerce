59b6ad61e2b831dc1110e9075c4ebb74e2dc07e5
Hawa37 <hawa.coulibaly@edu.ece.fr>
2021-05-28 23:52:51 +0200

    Update Notifications.php

---
+++ Notifications.php
@@ -0,0 +1,173 @@
+<?php 
+	session_start()
+	?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Notifications</title>
+	<meta charset="utf-8">
+	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="firstpage.css">
+	<link rel="stylesheet" type="text/css" href="Notifications.css">
+	<link rel="stylesheet"
+	 href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+	<script type="text/javascript">
+ 			$(document).ready(function(){
+ 			$('.header').height($(window).height());
+ 			});
+ 			
+	</script> 
+	<style type="text/css">
+		h1 {
+			text-align: center;
+			color: white;
+			background-color: blue;
+			padding: 20px;
+			width: 650px;
+			margin: 0 auto 20px auto;
+			border-radius: 5px;
+		}
+		table {
+			width: 680px;
+			margin: auto;
+		
+		}
+	</style>
+	
+</head>
+
+<body>
+	
+	<nav class="navbar navbar-expand-md">
+		 <a class="navbar-brand" href="index.php">
+		 	<div class="b-logo swift_left">
+                        <img src="logoShopECE.png" alt="ECE Store" class="img-fluid" width="150" height="200">
+            </div>
+		 </a>
+		 <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+		 	<span class="navbar-toggler-icon"></span>
+		 </button>
+		 <div class="collapse navbar-collapse" id="main-navigation">
+			 <ul class="navbar-nav">
+				 <li class="nav-item"><a class="nav-link" href="Acceuil.php">Accueil</a></li>
+				 <li class="nav-item"><a class="nav-link" href="parcourir.php">Tout Parcourir</a></li>
+				 <li class="nav-item"><a class="nav-link" href="Notifications.php">Notifications</a></li>
+				 <li class="nav-item"><a class="nav-link" href="Panier.php">Panier</a></li>
+				 <li class="nav-item"><a class="nav-link" href="index2.php">Votre Compte</a></li>
+			 </ul>
+		 </div>
+ 	</nav>
+ 	<h1>Vos critères</h1>
+ 	<table border="1">
+ 	<tr>
+		<td>Que recherchez-vous ?:</td>
+		<td>
+			 <input type="radio" name="choix" value="1">Meubles<br>
+			 <input type="radio" name="choix" value="2">Ordinateurs<br>
+			 <input type="radio" name="choix" value="3">Colliers<br>
+			 <input type="radio" name="choix" value="4">Voitures<br>
+			 <input type="radio" name="choix" value="5">Tableaux<br>
+			 <input type="radio" name="choix" value="6">Cahier<br>
+			 <input type="radio" name="choix" value="7">Montres<br>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			 <input type="submit" name="button1" value="Soumettre">
+		</td>
+	</tr>
+	</table>
+
+<?php
+
+$database = "eceshop";
+$db_handle = mysqli_connect('localhost', 'root', '');
+$db_found = mysqli_select_db($db_handle, $database);
+
+$choice = isset($_POST["choix"])? $_POST["choix"] : "";
+if (empty($choice)) {
+	$choice = 0;
+	}
+$choice = (int)$choice;
+$sql = "";
+
+if ($db_found) {
+	
+	switch ($choice) {
+		case 1:
+		$sql = "SELECT * FROM articles WHERE nom LIKE '%MEUBLE%'";
+		break;
+		case 2:
+		$sql = "SELECT * FROM articles WHERE nom LIKE '%ORDINATEUR%'";
+		break;
+		case 3:
+		$sql = "SELECT * FROM articles WHERE nom LIKE '%COLLIER%'";
+		break;
+		case 4:
+		$sql = "SELECT * FROM articles WHERE nom LIKE '%VOITURE%'";
+		break;
+		case 5:
+		$sql = "SELECT * FROM articles WHERE nom LIKE 'T%'";
+		break;
+		case 6:
+		$sql = "SELECT * FROM articles WHERE nom LIKE '%CAHIER%'";
+		break;
+		case 7:
+		$sql = "SELECT * FROM articles WHERE nom LIKE '%MONTRE%'";
+		break;
+		default:
+		$sql = "SELECT * FROM articles WHERE nom LIKE 'T%'";
+		break;
+}
+echo "<h2>Voici votre sélection</h2>";
+
+echo "<p> Résultat:</p>";
+$result = mysqli_query($db_handle, $sql);
+
+echo '<div class="container">';
+echo '<div class="row">';
+while ($data = mysqli_fetch_assoc($result)) {
+echo	'<div class="col-lg-4">';	
+echo		'<div class="content">';
+echo			'<a class="article" href="pageArticle.php">';
+echo		 	'<img src = "data:image/jpd;base64,' . base64_encode($data['image']) . '" width = "75%"/>';
+echo           '</a>';
+echo		 '<h5>'.$data['nom'].'</h5>';
+echo		$data['prix'].'€';
+echo 		'  <button type="submit" name="add" class="btn btn-danger my-3">Ajouter au panier <i class="fas fa-shopping-cart"></i></button>';
+echo         '<input type="hidden" name="idarticle" >';
+		
+echo		'</div>';
+echo	'</div>';
+
+
+}
+echo 	'</div>';
+echo'</div>';
+
+} else {
+echo "<br>Votre produits n'est pas disponible, vous pouvez activer la cloche de notification qui vous informera s'il est à nouveau disponible";
+}
+
+
+mysqli_close($db_handle);
+?>
+
+
+	<footer class="page-footer">
+		 <div class="container">
+			 <div class="row">
+				 <div class="col-lg-8 col-md-8 col-sm-12">
+				 	
+				 </div>
+				</div>
+			 <div class="footer-copyright text-center">Copyright &copy; 2021 ECE Store
+			 
+		</div>
+	</footer>
+</body>
+</html>
