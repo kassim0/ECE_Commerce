97346f432f12372d9af3cff3fe13b03ff1f10926
kassim0 <kassim_haddad@yahoo.fr>
2021-05-28 23:53:18 +0200

    Update compte.php

--- compte.php
+++ compte.php
@@ -1,21 +1,52 @@
 <?php
-
+	session_start();
 	$database="eceshop";
 	$db_handle=mysqli_connect('localhost',"root",'');
 	$db_found= mysqli_select_db($db_handle, $database);
 
-	$code=rand();
+	 $id=$_SESSION['ID'];
+	 $prenom=$_SESSION['prenom'];
+	 $nom=$_SESSION['nom'];
+	 $pseudo=$_SESSION['pseudo'];
+	 $email=$_SESSION['email'];
+	 $mdp= $_SESSION['mdp'];
+
+
+
+
+
+
+
+	$code=isset($_POST["code"])? $_POST["code"] : "";
+	$verif=0;
 	
 
+	if($db_found){
+		$sql="SELECT code FROM code";
+		$result = mysqli_query($db_handle, $sql);
+		while($data = mysqli_fetch_assoc($result)){
+	 		   if($code==$data['code']){
+	 		   	$verif=1;
 
-if(isset($_POST['signin'])){
+	 		   }//else{echo"le code est érroné<br>";}
 
-echo $code;
+	 	}
+
+	 		   if($verif==1){
+	 		   	$sql="INSERT INTO vendeur(nom,prenom,email,adresse,codepostal,ville,telephone,pseudo,mdp,pdp) VALUES ('$nom','$prenom','$email','',0,'',0,'$pseudo','$mdp',0)";
+	 		   	$result = mysqli_query($db_handle, $sql);
+	 		   	 echo "Inscription reussie, <br>cliquez "."<a href='#'>"."ici"."</a>"." pour rejoindre votre page<br>";
 
 
+	 		   	 $sql="DELETE FROM acheteur WHERE id_utilisateur = $id";
+	 		   	 $result = mysqli_query($db_handle, $sql);
 
-}//else isset signin
 
+	 		   }else{echo "le code est érroné.<br>";}
+
+	    }
+
+	
 
 
 
