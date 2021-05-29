11ff14dfe1cd82e1bcdbe46d6e353f0e5904cc2b
kassim0 <kassim_haddad@yahoo.fr>
2021-05-29 16:09:48 +0200

    Update Session.php

--- Session.php
+++ Session.php
@@ -10,6 +10,10 @@
 	$pseudo=isset($_POST["pseudo"])? $_POST["pseudo"] : "";
 	$email=isset($_POST["email"])? $_POST["email"] : "";
 	$mdp=isset($_POST["mdp"])? $_POST["mdp"] : "";
+	$adresse=isset($_POST["adresse"])? $_POST["adresse"] : "";
+	$codepostal=isset($_POST["codepostal"])? $_POST["codepostal"] : "";
+	$ville=isset($_POST["ville"])? $_POST["ville"] : "";
+	$tel=isset($_POST["tel"])? $_POST["tel"] : "";
 	$emailexist=0;
 	$nb=0;
 	$code=rand();
@@ -42,17 +46,10 @@ $database="eceshop";
 				$sql="SELECT email FROM acheteur";
 				$result = mysqli_query($db_handle, $sql);
 				while($data = mysqli_fetch_assoc($result)){
-
 					if($data['email']==$email){
-
-						
 						$emailexist=1;
-
 					}
-
 				}
-
-
 				if($emailexist==0){
 					$sql="INSERT INTO acheteur(nom,prenom,email,adresse,codepostal,ville,telephone,pseudo,mdp,pdp) VALUES ('$nom','$prenom','$email','',0,'',0,'$pseudo','$mdp',0)";
 					$result = mysqli_query($db_handle, $sql);
@@ -61,12 +58,9 @@ $database="eceshop";
 					 $sql="SELECT id_utilisateur FROM acheteur WHERE email='$email' AND mdp='$mdp' ";
 					 $result = mysqli_query($db_handle, $sql);
 					 while($data = mysqli_fetch_assoc($result)){
-
 					 echo "De plus votre ID est ".$data['id_utilisateur']."<br>";
 					 $ID=$data['id_utilisateur'];
 					 echo "Et dans le variable on a $ID";
-
-
 					}
 				}else{echo "email deja existant<br>";}
 			}else{echo "le formulaire n'est pas remplie<br>";}
@@ -77,20 +71,23 @@ $database="eceshop";
 
 
 
+
+
+
+
+
+
 		if(isset($_POST["connexion"])){		
 		if($db_found){
 			$sql="SELECT email,mdp FROM acheteur";
 			$result = mysqli_query($db_handle, $sql);
 			while($data = mysqli_fetch_assoc($result)){
-
 				if($data['email']==$email){
-
 					if($data['mdp']==$mdp){
 						$nb=1;
 					}
 				}
 			}
-
 		}else{echo "Fichier introuvable";}
 		if($nb==0){
 		echo "Email ou mot de passe invalide<br>";
@@ -107,17 +104,32 @@ $database="eceshop";
 
 
 
+	if(isset($_POST["enregistre"])){
+		echo "le bouton à été appuyé";
+		
+
+	}
+
+
+
+
+
+
 	$_SESSION['email']=$email;
     $_SESSION['mdp']=$mdp;
     if($db_found){
     	if($email!='' || $mdp!=''){
-    	$sql="SELECT id_utilisateur, nom, prenom, pseudo FROM acheteur WHERE email='$email' AND mdp='$mdp'";
+    	$sql="SELECT id_utilisateur, nom, prenom, pseudo, adresse, codepostal, ville, telephone FROM acheteur WHERE email='$email' AND mdp='$mdp'";
     	$result = mysqli_query($db_handle, $sql);
     	while($data = mysqli_fetch_assoc($result)){
     		$_SESSION['nom']=$data['nom'];
     		$_SESSION['prenom']=$data['prenom'];
     		$_SESSION['pseudo']=$data['pseudo'];
     		$_SESSION['ID']=$data['id_utilisateur'];
+    		$_SESSION['adresse']=$data['adresse'];
+    		$_SESSION['codepostal']=$data['codepostal'];
+    		$_SESSION['ville']=$data['ville'];
+    		$_SESSION['tel']=$data['telephone'];
 
     	}
     }
