9cc9038fc5a7f3f6999b065bc4e5387ff37c0962
kassim0 <kassim_haddad@yahoo.fr>
2021-05-29 20:05:23 +0200

    Update Session.php

--- Session.php
+++ Session.php
@@ -5,6 +5,7 @@
 
 
     $ID=0;
+    $choix=isset($_POST["choix"])? $_POST["choix"] : "";
 	$nom=isset($_POST["nom"])? $_POST["nom"] : "";
 	$prenom=isset($_POST["prenom"])? $_POST["prenom"] : "";
 	$pseudo=isset($_POST["pseudo"])? $_POST["pseudo"] : "";
@@ -14,6 +15,8 @@
 	$codepostal=isset($_POST["codepostal"])? $_POST["codepostal"] : "";
 	$ville=isset($_POST["ville"])? $_POST["ville"] : "";
 	$tel=isset($_POST["tel"])? $_POST["tel"] : "";
+	$table="acheteur";
+	$page="pageacheteur.php";
 	$emailexist=0;
 	$nb=0;
 	$code=rand();
@@ -43,7 +46,7 @@ $database="eceshop";
 	if(isset($_POST["inscription"])){
 		if($db_found){
 			if($nom!="" && $prenom!="" && $pseudo!="" && $email!="" && $mdp!=""){
-				$sql="SELECT email FROM acheteur";
+				$sql="SELECT email FROM $table";
 				$result = mysqli_query($db_handle, $sql);
 				while($data = mysqli_fetch_assoc($result)){
 					if($data['email']==$email){
@@ -53,15 +56,14 @@ $database="eceshop";
 				if($emailexist==0){
 					$sql="INSERT INTO acheteur(nom,prenom,email,adresse,codepostal,ville,telephone,pseudo,mdp,pdp) VALUES ('$nom','$prenom','$email','',0,'',0,'$pseudo','$mdp',0)";
 					$result = mysqli_query($db_handle, $sql);
-					//echo "regarde la BDD<br>";
 					 echo "Inscription reussie, <br>cliquez "."<a href='compte1.php'>"."ici"."</a>"." pour choisir votre compte<br>";
-					 $sql="SELECT id_utilisateur FROM acheteur WHERE email='$email' AND mdp='$mdp' ";
-					 $result = mysqli_query($db_handle, $sql);
-					 while($data = mysqli_fetch_assoc($result)){
-					 echo "De plus votre ID est ".$data['id_utilisateur']."<br>";
-					 $ID=$data['id_utilisateur'];
-					 echo "Et dans le variable on a $ID";
-					}
+					 // $sql="SELECT id_utilisateur FROM acheteur WHERE email='$email' AND mdp='$mdp' ";
+					 // $result = mysqli_query($db_handle, $sql);
+					 // while($data = mysqli_fetch_assoc($result)){
+					 // echo "De plus votre ID est ".$data['id_utilisateur']."<br>";
+					 // $ID=$data['id_utilisateur'];
+					 // echo "Et dans le variable on a $ID";
+					//}
 				}else{echo "email deja existant<br>";}
 			}else{echo "le formulaire n'est pas remplie<br>";}
 		}else{echo "fichier introuvable<br>";}
@@ -70,6 +72,7 @@ $database="eceshop";
 
 
 
+		
 
 
 
@@ -77,9 +80,27 @@ $database="eceshop";
 
 
 
-		if(isset($_POST["connexion"])){		
+		if(isset($_POST["connexion"])){	
+				echo $choix;
+			
+			if($choix==1){
+				$table="acheteur";
+				$page="pageacheteur.php";
+			}
+			if($choix==2){
+				$table="vendeur";
+				$page="pagevendeur.php";
+			}
+			if($choix==3){
+				$table="administrateur";
+				$page="pageadmin.php";
+			}
+
+
+
+
 		if($db_found){
-			$sql="SELECT email,mdp FROM acheteur";
+			$sql="SELECT email,mdp FROM $table";
 			$result = mysqli_query($db_handle, $sql);
 			while($data = mysqli_fetch_assoc($result)){
 				if($data['email']==$email){
@@ -91,12 +112,12 @@ $database="eceshop";
 		}else{echo "Fichier introuvable";}
 		if($nb==0){
 		echo "Email ou mot de passe invalide<br>";
-		 echo "<a href='connexion.html'>"."retourner à la page de connexion"."</a>";
+		 echo "<a href='connexion1.php'>"."retourner à la page de connexion"."</a>";
 		//echo "retourner à la page de connexion";
 		}
 		else{
 			echo "connexion réussie<br>";
-			 echo "Cliquez "."<a href='votrecompte.php'>"."ici"."</a>"." pour accéder à votre compte";
+			 echo "Cliquez "."<a href=$page>"."ici"."</a>"." pour accéder à votre compte";
 	}
 
 
@@ -104,19 +125,9 @@ $database="eceshop";
 
 
 
-	if(isset($_POST["enregistre"])){
-		echo "le bouton à été appuyé";
-		
-
-	}
 
 
-
-
-
-
-	$_SESSION['email']=$email;
-    $_SESSION['mdp']=$mdp;
+	
     if($db_found){
     	if($email!='' || $mdp!=''){
     	$sql="SELECT id_utilisateur, nom, prenom, pseudo, adresse, codepostal, ville, telephone FROM acheteur WHERE email='$email' AND mdp='$mdp'";
@@ -125,14 +136,18 @@ $database="eceshop";
     		$_SESSION['nom']=$data['nom'];
     		$_SESSION['prenom']=$data['prenom'];
     		$_SESSION['pseudo']=$data['pseudo'];
+    		$_SESSION['email']=$email;
+    		$_SESSION['mdp']=$mdp;
     		$_SESSION['ID']=$data['id_utilisateur'];
     		$_SESSION['adresse']=$data['adresse'];
     		$_SESSION['codepostal']=$data['codepostal'];
     		$_SESSION['ville']=$data['ville'];
     		$_SESSION['tel']=$data['telephone'];
 
+
     	}
     }
+
     }
 	
 
